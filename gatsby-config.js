ffc00fc8127088ed7c4c54f0118766e0e3915e2d
Robert Cobb <rwcobbjr@gmail.com>
2019-07-05 16:54:34 -0400

    switch favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
+        icon: `src/assets/rob-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
